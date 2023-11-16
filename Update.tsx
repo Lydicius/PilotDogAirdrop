40c8123b9cb8ac917b24beafff03e6af316805c2
Lydicius <louis.jonathan88@gmail.com>
2023-11-16 00:32:06 +0000

    ⚫️ auto commit

--- Update.tsx
+++ Update.tsx
@@ -11123,3 +11123,7 @@ curl: (22) The requested URL returned error: 408
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    209      0 --:--:-- --:--:-- --:--:--   209
 {"code":"InvalidParameters","error":"not found","data":null}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    920      0 --:--:-- --:--:-- --:--:--   923
+{"code":"InvalidParameters","error":"not found","data":null}
